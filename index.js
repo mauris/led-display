3f20f5bd3f39d5dc3e0a34282bbf8f04840f3876
Sam-Mauris Yong <mauris@hotmail.sg>
2017-07-13 18:02:54 +0800

    updated to show just message

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(0, 0x20);
 
-lcd.backlight(lcd.colors.BLUE);
+lcd.backlight(lcd.colors.RED);
 lcd.message('Hello World!');
-
-lcd.on('button_change', function(button) {
-  lcd.clear();
-  lcd.message('Button changed:\n' + lcd.buttonName(button));
-});
