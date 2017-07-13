38d5582ac5c31cf2b7030f822976812e07ad9625
Sam-Mauris Yong <mauris@hotmail.sg>
2017-07-13 18:39:54 +0800

    fixed turning on backlight

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(0, 0x20);
 
-lcd.sendBytes(0, 0x1F);
+lcd.backlight(0x07);
 
 var server = net.createServer((socket) => {
   socket.on('data', (data) => {
