64ffa9644558ba5ccffe1ded04d63f2a672dbe77
Sam-Mauris Yong <mauris@hotmail.sg>
2017-07-13 18:38:50 +0800

    updated code to start tcp server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+var net = require('net');
 var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(0, 0x20);
 
 lcd.sendBytes(0, 0x1F);
-lcd.message('Hello World!');
 
-lcd.on('button_change', function(button) {
-  lcd.clear();
-  lcd.message('Button changed:\n' + lcd.buttonName(button));
+var server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    lcd.message(data);
+  })
 });
+
+server.listen(1337, '127.0.0.1');
