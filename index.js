142630947672929bfe4db0a94750eb1ca601890e
Sam-Mauris Yong <mauris@hotmail.sg>
2017-07-13 18:14:32 +0800

    updated backlight order

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(0, 0x20);
 
-lcd.backlight(lcd.colors.BLUE);
 lcd.message('Hello World!');
+lcd.backlight(lcd.colors.BLUE);
 
 lcd.on('button_change', function(button) {
   lcd.clear();
