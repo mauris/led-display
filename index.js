16feed0501bb1b93373a4bd5e44c65aef8760512
Sam-Mauris Yong <mauris@hotmail.sg>
2017-07-13 16:30:58 +0800

    added backlight

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(0, 0x20);
 
+lcd.backlight(lcd.colors.BLUE);
 lcd.message('Hello World!');
 
 lcd.on('button_change', function(button) {
