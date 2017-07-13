b43e759e186c28e28db8e7b7ff47c98e0ad58bf2
Sam-Mauris Yong <mauris@hotmail.sg>
2017-07-14 06:43:57 +0800

    added timer to on / off backlight

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const TURN_ON = 0x07;
+const TURN_OFF = 0x00;
+const BACKLIGHT_DELAY = 5000; // ms
+const LISTEN_PORT = 1337;
+
 var net = require('net');
 var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(0, 0x20);
 
-lcd.backlight(0x07);
+var timer;
 
 var server = net.createServer((socket) => {
   socket.name = socket.remoteAddress + ":" + socket.remotePort;
@@ -12,8 +17,14 @@ var server = net.createServer((socket) => {
   socket.on('data', (data) => {
     console.log('[' + socket.name + ']: ' + data);
     lcd.clear()
+    lcd.backlight(TURN_ON);
     lcd.message(data);
+
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      lcd.backlight(TURN_OFF);
+    }, BACKLIGHT_DELAY);
   })
 });
 
-server.listen(1337);
+server.listen(LISTEN_PORT);
