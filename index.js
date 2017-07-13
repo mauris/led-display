c810efbe86ba2ae79555f38222d18dd0465d0859
Sam-Mauris Yong <mauris@hotmail.sg>
2017-07-13 18:03:06 +0800

    changed lcd colour

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var LCDPLATE, lcd;
 LCDPLATE = require('adafruit-i2c-lcd').plate;
 lcd = new LCDPLATE(0, 0x20);
 
-lcd.backlight(lcd.colors.RED);
+lcd.backlight(lcd.colors.BLUE);
 lcd.message('Hello World!');
